76d47861d73f047ed4f853585c351d6712188ad2
Dominik Keisz <dominik.keisz@cheppers.com>
2020-07-11 16:19:43 +0200

    Created User model and started  mongodb setup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const dbConnection = String(process.env.DB_CONNECTION);
+const mongoose = require('mongoose');
+// Import routes
+const authRouter = require('./backend/api-routes/auth');
+console.log(dbConnection);
+// Connect to db
+mongoose.connect(
+  dbConnection,
+  { useNewUrlParser: true },
+  (err) => {
+    if (err) {
+      console.error('Error occured while connection to DB!');
+    } else {
+      console.log('Datbase connection established successfully');
+    }
+  }
+);
 
-app.get('/', (req, res) => {
-  res.send('Hello Nodemon!');
-});
+// Middlewares
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`App is listening on http://localhost:${port}`);
-});
+// Route middlawares
+app.use('/api/user', authRouter);
+
+app.listen(
+  port,
+  () => console.log(`App is listening on http://localhost:${port}`)
+);
