18ae716e9904f0673ddc0353e2754b4a5f300822
Dominik Keisz <dominik.keisz@cheppers.com>
2020-07-16 17:30:37 +0200

    Further user validation

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     min: 4,
+    unique: true,
   },
   email: {
     type: String,
-    required: true,
-    max: 255,
-    min: 6,
+    lowercase: true,
+    required: [true, "can't be blank"],
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+    index: true,
+    unique: true,
   },
   password: {
     type: String,
@@ -22,6 +26,8 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
-});
+}, {timestamps: true});
+
+userSchema.plugin(uniqueValidator, { message: " is already taken."});
 
 module.exports = mongoose.model('User', userSchema);
