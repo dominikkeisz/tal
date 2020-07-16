e838d140e2879d088e597f743990a23f456da55a
Dominik Keisz <dominik.keisz@cheppers.com>
2020-07-16 17:24:19 +0200

    Deleted auth test, will need to setup functional testing later

--- src/start.js
+++ src/start.js
@@ -2,5 +2,5 @@ const app = require('./backend/server');
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log(`App is listening on http://localhost:${port}`);
+  console.log(`App is listening on http://localhost:${port}`);
 });
